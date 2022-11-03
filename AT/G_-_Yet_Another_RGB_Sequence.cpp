9c425f272cbbbfc8a51eedf4ccb1a06464a2c6ae
purple-vine <2653879495@qq.com>
2022-11-03 21:20:18 +0800

    upd abc???G

---
+++ AT/G_-_Yet_Another_RGB_Sequence.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+using namespace std;
+const int mod = 998244353, M = 3000005;
+int qpow(int a, int b){
+    long long ans = 1ll;
+    for(; b; b >>= 1) {if(b & 1) ans = 1ll * ans * a % mod; a = 1ll * a * a % mod;}
+    return ans;
+}
+int inv(int k) {return qpow(k, mod - 2);}
+int addd(int a, int b) {a += b; return a > mod ? a-mod : a;}
+int minus(int a, int b) {a -= b; return a < 0 ? a+mod : a;}
+void add(int &x, int y) {x += y; if(x > mod) x -= mod;}
+int fact(int x) {int ans = 1; for(int i = 1; i <= x; i++) ans = 1ll * ans * i % mod; return ans;}
+int fac[M], invfac[M], invn[M];
+void pre(int n = 250000){
+    fac[0] = invfac[0] = fac[1] = invfac[1] = invn[1] = 1;
+    for(int i = 2; i <= n; i++)
+        invn[i] = 1ll * (mod - mod/i) * invn[mod%i] % mod,
+        fac[i] = 1ll * fac[i-1] * i % mod,
+        invfac[i] = 1ll * invfac[i-1] * invn[i] % mod;
+}
+int C(int n, int m) {return m > n ? 0 : 1ll * fac[n] * invfac[m] % mod * invfac[n-m] % mod;}
+int main(){
+    int a, b, c, d; scanf("%d %d %d %d", &a, &b, &c, &d); pre(3000000);
+    int n = a-d, m = b-d, k = d+c; printf("%d\n", 1ll * C(n+k, k) * C(m+k, k) % mod * C(k, c) % mod);
+}
